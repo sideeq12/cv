bc3ac36d9690305b5172fe3b680b802056958d8d
sideeq12 <abdwaheed2018@gmail.com>
2020-08-30 10:22:22 -0700

    remain footer and nav

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-var burger = document.querySelector('#burger');
+var burger = document.querySelector('.burger');
 burger.addEventListener('click',function(){
     let list = document.querySelector('ul')
     list.classList.toggle('active')
 } )
 
-var skills = document.querySelector('#resume')
-skills.addEventListener('click', function(){
-    var display1 = document.querySelector('#my_first');
-    display1.classList.toggle('resume')
-})
-
-// var home = document.querySelector('#hire')
-// home.addEventListener('click', function(){
-//     display1.classList.toggle("hire")
-// })
